2a896a720e3704c7e35add88797ded17cc084923
Himanshu Agarwal <himanshuagarwal@MB-M-HIMANSHUA.local>
2023-09-22 19:10:41 +0530

    karbon card assignment

--- src/Helpers.tsx
+++ src/Helpers.tsx
@@ -1,7 +1,7 @@
 import { User } from "./Interface"
 
 export function userFormValidator(name: string, age: string, dob: string, gender: string, food: string, hobbies: string){
-    if(!name?.trim() || !parseInt(age) || parseInt(age) < 0 || parseInt(age) > 200 || !dob || !gender || !food || !hobbies){
+    if(!name?.trim() || !parseInt(age) || parseInt(age) < 0 || parseInt(age) > 200 || !dob || !gender || !food || !hobbies || hobbies === "--Please choose an option--"){
         alert("Enter all the fields/ valid fields")
         throw new Error("Enter all the fields/ valid fields")
     }
